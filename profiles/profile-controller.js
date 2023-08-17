a93de2f0708c1acd2d7aa28bc20c55890a765421
dchioffi <115377576+dchioffi@users.noreply.github.com>
2023-08-17 17:02:39 -0400

    Update profile-controller.js

--- profiles/profile-controller.js
+++ profiles/profile-controller.js
@@ -39,10 +39,91 @@ const updateProfile = async (req, res) => {
   }
 }
 
+const followUser = async (req, res) => {
+  const { userIdToFollow } = req.params;
+  const userId = req.user._id;
+  const result = await profileDao.followUser(userId, userIdToFollow);
+
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(500).send('Error following user.');
+  }
+};
+
+const unfollowUser = async (req, res) => {
+  const { userIdToUnfollow } = req.params;
+  const userId = req.user._id;
+  const result = await profileDao.unfollowUser(userId, userIdToUnfollow);
+
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(500).send('Error unfollowing user.');
+  }
+};
+
+const favoriteLocation = async (req, res) => {
+  const { locationId } = req.params;
+  const userId = req.user._id;
+  const result = await profileDao.favoriteLocation(userId, locationId);
+
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(500).send('Error favoriting location.');
+  }
+};
+
+const unfavoriteLocation = async (req, res) => {
+  const { locationId } = req.params;
+  const userId = req.user._id;
+  const result = await profileDao.unfavoriteLocation(userId, locationId);
+
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(500).send('Error unfavoriting location.');
+  }
+};
+
+const getLoggedInUserProfile = async (req, res) => {
+  const userId = req.user._id;
+  const profile = await profileDao.findProfileByUserId(userId);
+
+  if (profile) {
+    // Include sensitive information for the logged-in user
+    res.json(profile);
+  } else {
+    res.status(404).send('Profile not found.');
+  }
+};
+
+const getOtherUserProfile = async (req, res) => {
+  const { profileId } = req.params;
+  const profile = await profileDao.findProfileByUserId(profileId);
+
+  if (profile) {
+    // Exclude sensitive information for other users
+    const { email, phone, ...publicProfile } = profile;
+    res.json(publicProfile);
+  } else {
+    res.status(404).send('Profile not found.');
+  }
+};
+
+
 
 const ProfileController = (app) => {
   app.get('/profile/:profileId', getProfile);
   app.put('/profile', updateProfile);
+  app.put('/profile/follow/:userIdToFollow', followUser);
+  app.put('/profile/unfollow/:userIdToUnfollow', unfollowUser);
+  app.put('/profile/favorite/:locationId', favoriteLocation);
+  app.put('/profile/unfavorite/:locationId', unfavoriteLocation);
+  app.get('/profile', getLoggedInUserProfile);
+  app.get('/profile/:profileId', getOtherUserProfile);
+
   // ... add other profile routes as needed
 }
 
